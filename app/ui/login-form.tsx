5541955833fc27c803543d962150190d2b500f18
fconunezalonso <fconunezdev@outlook.com>
2024-06-05 14:12:52 -0600

    Login stablished

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -79,6 +79,8 @@ export default function LoginForm() {
 }
 
 function LoginButton() {
+  const { pending } = useFormStatus();
+
   return (
     <Button className="mt-4 w-full">
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
